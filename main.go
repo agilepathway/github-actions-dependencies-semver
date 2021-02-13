d3ec05b3fe1f2fb70d8d51f967c4a03072b5638d
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-02-13 02:00:05 +0000

    Add downloading of file

--- main.go
+++ main.go
@@ -2,13 +2,38 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 var Default = SemverLookup
 
 // SemverLookup provides the current semantic version associated with the given GitHub Action dependency
 func SemverLookup() error {
-	// https://github.com/agilepathway/agilepathway-template/raw/non-managed-dependencies/.github/workflows/non-package-managed-dependencies.yml
+	yaml_filename := "https://github.com/agilepathway/agilepathway-template/raw/non-managed-dependencies/.github/workflows/non-package-managed-dependencies.yml"
+	_, err := getContent(yaml_filename)
+
 	fmt.Print("v1.22.1")
-	return nil
+	return err
+}
+
+func getContent(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("GET error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Status error: %v", resp.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Read body: %v", err)
+	}
+
+	return data, nil
 }
